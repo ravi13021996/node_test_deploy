928ceb8f0c0c9c47de8d460a94c87dab80088541
ravikant <ravikant@meradoc.com>
2025-05-07 14:44:29 +0530

    added changes

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ let app = express()
 dotenv.config()
 app.use(express.json())
 console.log("testting test")
-app.post("/getSampleData",async (req, res) => {
-    try{
-
-        let resultData=await axios.get("https://jsonplaceholder.typicode.com/posts")
-        console.log("getSampleData resultData",resultData.data)
-        console.log("getSampleData", req.body)
-        res.send([
-            {
-                name: "ravikant",
-                address: "1st floor faridabad"
-            },
-            {
-                name: "shashikant",
-                address: "2nd floor faridabad"
-            }
-        ])
-    }catch(err){
-        console.log("getSampleData err",err)
-    }
-})
+    app.post("/getSampleData",async (req, res) => {
+        try{
+            console.log("getSampleData inner ")
+            let resultData=await axios.get("https://jsonplaceholder.typicode.com/posts")
+            console.log("getSampleData resultData",resultData.data)
+            console.log("getSampleData", req.body)
+            res.send([
+                {
+                    name: "ravikant",
+                    address: "1st floor faridabad"
+                },
+                {
+                    name: "shashikant",
+                    address: "2nd floor faridabad"
+                }
+            ])
+        }catch(err){
+            console.log("getSampleData err",err)
+        }
+    })
 
 let PORT=process.env.PORT
 app.get("/healthChek",(req,res)=>{
