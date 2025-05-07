12662c34adda13430838c56137e7ebd728f5a648
ravikant <ravikant@meradoc.com>
2025-05-07 13:38:27 +0530

    added changes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const { default: axios } = require("axios")
 let app = express()
 dotenv.config()
 app.use(express.json())
 console.log("testting test")
-app.post("/getSampleData", (req, res) => {
+app.post("/getSampleData",async (req, res) => {
+    let resultData=await axios.get("https://jsonplaceholder.typicode.com/posts")
+    console.log("getSampleData resultData",resultData.data)
     console.log("getSampleData", req.body)
     res.send([
         {
